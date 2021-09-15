7225fcc116fbbd56373f50dae841952137f17e01
Tiago Sampaiogit config --global user.name Tiago <teagolos10@gmail.com>
2021-09-15 17:24:59 -0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
 import logo from './logo.svg';
+import Githubimage from './GitHub.png';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import Footer from './components/Footer';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.github.com/users/${search}`)
+      .then(response => response.json())
+      .then(userResponse => setUserData(userResponse));
+  }
+
+  console.log(userData);
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setSearch(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1 className="py-4 text-uppercase">Github profile</h1>
+      <div>
+        <h5>Enter your nick</h5>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <div className="inpot-group">
+            <input
+              type="text"
+              className="form-control"
+              required
+              value={search}
+              onChange={handleChange}
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div className="py-2">
+        {!userData && (
+          <img
+            src={Githubimage}
+            className="responsive rounded-circle"
+            alt=""
+            height="200px"
+          />
+        )}
+        {userData && (
+          <div>
+            <img
+              src={userData.avatar_url}
+              className="responsive rounded-circle"
+              alt=""
+              height="200px"
+            />
+            <h1 >
+              <a href="https://github.com/SampTI" target="_new">
+                {userData.name}
+              </a>
+            </h1>
+            <h3>{userData.location}</h3>
+            <p>
+              <a href={userData.blog} target="_new" className="text-info">
+                {userData.blog}
+              </a>
+            </p>
+          </div>
+        )}
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/tiago-sampaio-9a5982208" target="_blank" rel="noreferrer">Tiago Ap. Sampaio</a></p>
+                </div>
+        </footer>
+    );
+}
+
+export default Footer;
